4d0dd0207c9f6df6837c80d9b6de647826802a4c
lothfy <lothfy.sabour@ext.leroymerlin.fr>
2023-04-02 01:02:30 +0200

    fix: 🐛 "start": "node dist/main.js"

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
   });
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(8080);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
